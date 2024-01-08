adb3651a66436f822a50fad6618f5e3058a755d4
thomas-spinec <thomas.spinec@laplateforme.io>
2024-01-08 11:05:19 +0100

    Add Product class and index.php file

---
+++ job-01/Product.php
@@ -0,0 +1,65 @@
+<?php
+
+class Product
+{
+    private ?int $id;
+    private ?string $name;
+    private ?array $photos;
+    private ?int $price;
+    private ?string $description;
+    private ?int $quantity;
+    private ?DateTime $createdAt;
+    private ?DateTime $updatedAt;
+
+    public function __construct(?int $id, ?string $name, ?array $photos, ?int $price, ?string $description, ?int $quantity, ?DateTime $createdAt, ?DateTime $updatedAt)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->photos = $photos;
+        $this->price = $price;
+        $this->description = $description;
+        $this->quantity = $quantity;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
