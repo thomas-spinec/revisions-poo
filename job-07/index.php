091763db55a2b09f6df4526ee3efe12f4ce7ddd1
thomas-spinec <thomas.spinec@laplateforme.io>
2024-01-10 14:53:56 +0100

    get a product by an id

---
+++ job-07/index.php
@@ -0,0 +1,99 @@
+<?php
+// connect to database pdo
+$dbname = 'draft-shop';
+$host = 'localhost';
+$dbuser = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $dbuser, $password);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+
+
+require_once 'Product.php';
+require_once 'Category.php';
+
+$product = new Product();
+$product->setPdo($pdo);
+$result = $product->findOneById(4); // get a product
+// $result = $product->findOneById(9); // false
+
+var_dump($result);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job06</title>
+    <style>
+        th,
+        td {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+
+        img {
+            width: 100px;
+        }
+
+        th {
+            background-color: lightgrey;
+        }
+
+
+
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Job06</h1>
+
+    <!-- <table>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>photos</th>
+                <th>price</th>
+                <th>description</th>
+                <th>quantity</th>
+                <th>createdAt</th>
+                <th>updatedAt</th>
+                <th>category_id</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?= $product->getId() ?></td>
+                <td><?= $product->getName() ?></td>
+                <td>
+                    <?php foreach ($product->getPhotos() as $photo) : ?>
+                        <img src="<?= $photo ?>" alt="">
+                    <?php endforeach ?>
+                </td>
+                <td><?= $product->getPrice() ?></td>
+                <td><?= $product->getDescription() ?></td>
+                <td><?= $product->getQuantity() ?></td>
+                <td><?= $product->getCreatedAt()->format('d-m-Y') ?></td>
+                <td><?= $product->getUpdatedAt()->format('d-m-Y') ?></td>
+                <td><?= $product->getId_category() ?></td>
+            </tr>
+        </tbody>
+    </table> -->
+
+</body>
+
+</html>
