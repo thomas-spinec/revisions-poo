04a726327a6d6e442ef9c905ad1e28b6a410b20d
thomas-spinec <thomas.spinec@laplateforme.io>
2024-01-11 11:29:19 +0100

    Add Clothing class, index.php, and Category class

---
+++ job-11/Clothing.php
@@ -0,0 +1,11 @@
+<?php
+
+class Clothing extends Product
+{
+
+    // size, color, type, material_fee
+    public function __construct(protected ?int $id = null, protected ?string $name = null, protected ?string $description = null, protected ?float $price = null, protected ?string $image = null, protected ?DateTime $createdAt = null, protected ?DateTime $updatedAt = null, protected ?int $id_category = null, protected ?string $size = null, protected ?string $color = null, protected ?string $type = null, protected ?int $materialFee = null)
+    {
+        parent::__construct($id, $name, $description, $price, $image, $createdAt, $updatedAt, $id_category);
+    }
+}
